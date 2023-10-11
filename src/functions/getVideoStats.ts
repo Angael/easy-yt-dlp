f5aeda5ab8dd55b30bf472b30a25e49d1bb69b51
Angael <krzysztofwidacki@gmail.com>
2023-10-12 00:19:38 +0200

    fix getVideoStats, maybe

--- src/functions/getVideoStats.ts
+++ src/functions/getVideoStats.ts
@@ -1,15 +1,6 @@
 import child from "child_process";
 import { VideoStats } from "./getVideoStats.types";
 
-const isJson = (text: string) => {
-  try {
-    JSON.parse(text);
-    return true;
-  } catch {
-    return false;
-  }
-};
-
 export const getVideoStats = (
   ytDlpPath: string,
   link: string
@@ -17,18 +8,11 @@ export const getVideoStats = (
   new Promise(async (res, rej) => {
     const ytDlpProcess = child.spawn(ytDlpPath, [link, "-j", "--no-warnings"]);
 
-    let stats = {};
-
-    let out: string[] = [];
+    const out: string[] = [];
     ytDlpProcess.stdout.on("data", (buffer: Buffer) => {
       const text = buffer.toString().trim();
-      out.push(text);
       if (!text) return;
-
-      if (isJson(text)) {
-        // hopefully only one json object is written to stdout
-        stats = JSON.parse(text);
-      }
+      out.push(text);
     });
 
     const errors: string[] = [];
@@ -43,10 +27,16 @@ export const getVideoStats = (
         rej(new Error([...out, ...errors].join("\n")));
       }
 
-      if (Object.keys(stats).length) {
-        res(stats as VideoStats);
-      } else {
-        rej(new Error([...out, ...errors].join("\n")));
+      const outJson = out.join("");
+      try {
+        const stats = JSON.parse(outJson) as VideoStats;
+        res(stats);
+      } catch (e: any) {
+        rej(
+          new Error(
+            ["Failed to parse JSON object", ...out, ...errors].join("\n")
+          )
+        );
       }
     });
   });
